c632bacaa73a3f2e94454cf8bd934a7a1d5113da
Iwark <iwark02@gmail.com>
2014-10-14 01:24:59 +0900

    quick test

--- ccltTests/ccltTests.swift
+++ ccltTests/ccltTests.swift
@@ -6,31 +6,32 @@
 //  Copyright (c) 2014 Donuts. All rights reserved.
 //
 
-import UIKit
-import XCTest
+import Quick
+import Nimble
+import cclt
+import Alamofire
+import SwiftyJSON
 
-class ccltTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+class TableOfContentsSpec: QuickSpec {
+    override func spec() {
+        
+        describe("the table of contents below") {
+            
+            it("Alamofire"){
+                
+                var summaries:JSON = JSON([["title":"test quick title"]])
+                
+                Alamofire.request(CcltRoute.Summaries()).response {
+                    (request, response, data, error) in
+                    
+                    if let json = data as? NSData {
+                        summaries = JSON(data: json)
+                    }
+                }
+                
+                expect(summaries.array![0]["title"]).toEventuallyNot(equal("test quick title"), timeout: 3)
+            }
+        
         }
     }
-    
 }
